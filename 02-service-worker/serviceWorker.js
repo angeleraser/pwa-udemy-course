2a75169b0ffb5f372fc7d04390acba01e7e06198
Angel Figuera <angel.figuera@osanasalud.com>
2020-12-20 12:40:48 -0400

    feat: resolve code task to fetch a new image in service worker fetch event listener

--- 02-service-worker/serviceWorker.js
+++ 02-service-worker/serviceWorker.js
@@ -1,22 +1,9 @@
 self.addEventListener("fetch", (event) => {
   const {
     request: { url },
-    request,
   } = event;
-  if (url.includes("style.css")) {
-    const customResponse = new Response(
-      `
-      body {
-        background-color: red;
-        color: #fff;
-      }
-    `,
-      {
-        headers: {
-          "Content-Type": "text/css",
-        },
-      }
-    );
-    event.respondWith(customResponse);
+  if (url.includes("main.jpg")) {
+    const customURL = "./img/main-patas-arriba.jpg";
+    event.respondWith(fetch(customURL));
   }
 });
